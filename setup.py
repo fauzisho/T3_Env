60e002849666ff5dfc430baa9160eb9d86612061
fauzisho <fauzisholichin@gmail.com>
2024-04-04 17:35:52 +0200

    up project version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
       name='gym_gui_tictactoe',
-      version='0.1',
+      version='0.1.1',
       description='An OpenAI gym environment for playing Tic Tac Toe',
       url='https://github.com/fauzisho/T3_Env',
       author='AI Trafic Project',
